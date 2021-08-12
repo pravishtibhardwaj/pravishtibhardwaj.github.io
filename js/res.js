754cd0fa4545aecc3caf3e270ff249759d84712b
pravishti bhardwaj <bhard.pravi08@gmail.com>
2021-08-13 00:57:08 +0530

    updated

--- js/res.js
+++ js/res.js
@@ -1,10 +1,4 @@
 burger=document.getElementById('burger')
-na=document.getElementById('name')
-em=document.getElementById('email')
-mes=document.getElementById('message')
 burger.addEventListener('click',()=>{
     nav.classList.toggle('visibility')
 }) 
-na.addEventListener('click',()=>{
-    // na.classList.toggle('det')
-})
