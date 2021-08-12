b8645be2d53a0cff9691fe17691c2be775ef871c
pravishti bhardwaj <bhard.pravi08@gmail.com>
2021-08-12 21:17:20 +0530

    some stylings are modified

--- js/res.js
+++ js/res.js
@@ -1,4 +1,10 @@
 burger=document.getElementById('burger')
+na=document.getElementById('name')
+em=document.getElementById('email')
+mes=document.getElementById('message')
 burger.addEventListener('click',()=>{
     nav.classList.toggle('visibility')
 }) 
+na.addEventListener('click',()=>{
+    // na.classList.toggle('det')
+})
